cd20bc16cc7f32c9628a22d163eea9e352827aaf
lamnhan066 <hello@lamnhan.dev>
2024-06-26 23:06:29 +0700

    Migrate to package web to support wasm

--- lib/g_recaptcha_v3_web.dart
+++ lib/g_recaptcha_v3_web.dart
@@ -1,37 +1,44 @@
-@JS('grecaptcha')
 library g_recaptcha_v3_web;
 
 import 'dart:async';
-import 'dart:html' as html;
+import 'dart:js_interop';
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
-import 'package:js/js.dart';
-import 'package:js/js_util.dart' as js_util;
+import 'package:web/web.dart' as web;
 
-@JS('flutter_g_recaptcha_v3_key')
-external set _gRecaptchaV3Key(String key);
-@JS('flutter_g_recaptcha_v3_key')
-external String get _gRecaptchaV3Key;
-
-@JS('ready')
-external Future _ready(void Function() f);
-
-@JS('execute')
-external Future<String> _execute(String action, _Options options);
+@JS('grecaptcha')
+external Grecaptcha get grecaptcha;
 
 @JS()
 @anonymous
-class _Options {
+@staticInterop
+class Grecaptcha {}
+
+@staticInterop
+extension GRecaptchaExtension on Grecaptcha {
+  external JSPromise ready(JSFunction f);
+  external JSPromise<JSString> execute(JSString key, Options options);
+}
+
+@JS()
+@anonymous
+@staticInterop
+extension type Options._(JSObject _) implements JSObject {
+  external factory Options({String action});
+}
+
+extension OptionsExtension on Options {
   external String get action;
-  external factory _Options({String action});
 }
 
 /// A web implementation of the GRecaptchaV3 plugin.
 ///
 /// use `GRecaptchaV3` not ~GRecaptchaV3PlatformInterace~
 class GRecaptchaV3PlatformInterface {
+  static String? _gRecaptchaV3Key;
+
   static void registerWith(Registrar registrar) {
     final MethodChannel channel = MethodChannel(
       'g_recaptcha_v3',
@@ -57,31 +64,49 @@ class GRecaptchaV3PlatformInterface {
   /// This method should be called before calling `execute()` method.
   static Future<bool> ready(String key, bool showBadge) async {
     if (!kIsWeb) return false;
-    try {
-      _gRecaptchaV3Key = key;
-      await _ready(allowInterop(() {
-        debugPrint('gRecaptcha V3 ready');
-        changeVisibility(showBadge);
-      }));
-      return true;
-    } catch (e) {
-      debugPrint("Error: Looks like reCaptcha js is not loaded yet."
-          "Try to add the recaptcha js to your html <head> tag (or before flutter.js).");
-      debugPrint(e.toString());
+
+    _gRecaptchaV3Key = key;
+    if (await _waitForGrecaptchaReady()) {
+      changeVisibility(showBadge);
+      debugPrint('gRecaptcha V3 ready');
       return false;
     }
+
+    return true;
+  }
+
+  static Future<bool> _waitForGrecaptchaReady() async {
+    final completer = Completer<bool>();
+    void complete(bool isBreak) {
+      if (!completer.isCompleted) {
+        completer.complete(isBreak);
+      }
+    }
+
+    try {
+      await grecaptcha
+          .ready(() {
+            complete(true);
+          }.toJS)
+          .toDart;
+      complete(true);
+    } catch (e) {
+      await Future.delayed(const Duration(milliseconds: 20));
+      complete(false);
+    }
+    return completer.future;
   }
 
   /// use `GRecaptchaV3` not ~GRecaptchaV3PlatformInterace~
   static Future<String?> execute(String action) async {
     if (!kIsWeb) return null;
-    if (":$_gRecaptchaV3Key" == ':undefined') {
+    if (_gRecaptchaV3Key == null) {
       throw Exception('gRecaptcha V3 key not set : Try calling ready() first.');
     }
     try {
-      String? result = await js_util.promiseToFuture<String>(
-          await _execute(_gRecaptchaV3Key, _Options(action: action)));
-      return result;
+      final result =
+          grecaptcha.execute(_gRecaptchaV3Key!.toJS, Options(action: action));
+      return (await result.toDart).toDart;
     } catch (e) {
       debugPrint(e.toString());
       // Error: No reCAPTCHA clients exist.
@@ -92,7 +117,7 @@ class GRecaptchaV3PlatformInterface {
   /// change the reCaptcha badge visibility
   static Future<void> changeVisibility(bool showBagde) async {
     if (!kIsWeb) return;
-    var badge = html.document.querySelector(".grecaptcha-badge");
+    var badge = web.document.querySelector(".grecaptcha-badge") as dynamic;
     if (badge == null) return;
     badge.style.zIndex = "10";
     badge.style.visibility = showBagde ? "visible" : "hidden";

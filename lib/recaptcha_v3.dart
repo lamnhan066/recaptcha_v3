cb051d6fbbac4f3cbda2352b0f04f48cfc926353
lamnhan066 <hello@lamnhan.dev>
2024-06-27 10:19:29 +0700

    Add the `RecaptchaBrand` widget.

--- lib/recaptcha_v3.dart
+++ lib/recaptcha_v3.dart
@@ -1,3 +1,4 @@
 library recaptcha_v3;
 
 export 'src/recaptcha.dart';
+export 'src/recaptcha_brand.dart';
---
+++ lib/src/recaptcha_brand.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher_string.dart';
+
+class RecaptchaBrand extends StatelessWidget {
+  const RecaptchaBrand({
+    super.key,
+    this.normalTextColor = Colors.grey,
+    this.tappableTextColor = Colors.blue,
+    this.fontSize = 11,
+    this.margin = const EdgeInsets.symmetric(vertical: 0, horizontal: 8),
+  });
+
+  final Color normalTextColor;
+  final Color tappableTextColor;
+  final double fontSize;
+  final EdgeInsetsGeometry margin;
+
+  Future<bool> goToUrl(String url) async {
+    if (await canLaunchUrlString(url)) {
+      return await launchUrlString(url);
+    }
+
+    return false;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: margin,
+      child: Text.rich(
+        TextSpan(
+          text: 'This site is protected by reCAPTCHA and the Google\n',
+          children: [
+            TextSpan(
+              text: 'Privacy Policy',
+              style: TextStyle(color: tappableTextColor, fontSize: fontSize),
+              recognizer: TapGestureRecognizer()
+                ..onTap = () => goToUrl('https://policies.google.com/privacy'),
+            ),
+            const TextSpan(text: ' and '),
+            TextSpan(
+              text: 'Terms of Service',
+              style: TextStyle(color: tappableTextColor, fontSize: fontSize),
+              recognizer: TapGestureRecognizer()
+                ..onTap = () => goToUrl('https://policies.google.com/terms'),
+            ),
+            const TextSpan(text: ' apply.'),
+          ],
+          style: TextStyle(color: normalTextColor, fontSize: fontSize),
+        ),
+        textAlign: TextAlign.center,
+      ),
+    );
+  }
+}

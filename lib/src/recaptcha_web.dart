e83210cfc388b32aee48ee794b96db9c5e5649e0
lamnhan066 <hello@lamnhan.dev>
2024-07-24 23:56:57 +0700

    Refactor and add a delay to decrease the known issue

--- lib/src/recaptcha_web.dart
+++ lib/src/recaptcha_web.dart
@@ -1,6 +1,5 @@
 import 'dart:async';
 import 'dart:js_interop';
-import 'dart:js_interop_unsafe';
 
 import 'package:flutter/foundation.dart';
 import 'package:web/web.dart';
@@ -8,24 +7,13 @@ import 'package:web/web.dart';
 @JS('grecaptcha')
 external Grecaptcha get grecaptcha;
 
-@JS()
-@anonymous
-@staticInterop
-class Grecaptcha {}
-
-extension GRecaptchaExtension on Grecaptcha {
-  external JSPromise ready(JSFunction f);
+extension type Grecaptcha._(JSObject _) implements JSObject {
+  external JSVoid ready(JSFunction f);
   external JSPromise<JSString> execute(JSString key, Options options);
 }
 
-@JS()
-@anonymous
-@staticInterop
 extension type Options._(JSObject _) implements JSObject {
   external factory Options({JSString action});
-}
-
-extension OptionsExtension on Options {
   external JSString get action;
 }
 
@@ -34,72 +22,61 @@ extension OptionsExtension on Options {
 /// use `Recaptcha` not ~RecaptchaPlatformInterace~
 class RecaptchaImpl {
   static String? _recaptchaKey;
-  static final _completer = Completer<void>();
+  static final _readyCompleter = Completer<void>();
+  static bool _isReadyCalled = false;
   static final _isShowingBadge = ValueNotifier(false);
 
   /// Return `true` if the badge is showing.
   static ValueNotifier<bool> get isShowingBadge => _isShowingBadge;
 
   /// This method should be called before calling `execute()` method.
-  static Future<void> ready(String key, bool showBadge) async {
+  static Future<void> ready(
+    String key,
+    bool showBadge, {
+    // Delay before checking the `grecaptcha.ready` to avoid issue on
+    // Flutter stable (3.22.2) and beta (3.23.0-0.1.pre).
+    // You can change this value to `null` if you're using the `master` channel.
+    Duration? delay = const Duration(milliseconds: 300),
+  }) async {
     if (!kIsWeb) return;
-    if (_completer.isCompleted) return;
-
+    if (_isReadyCalled) return;
+    if (_readyCompleter.isCompleted) return;
+    _isReadyCalled = true;
     _recaptchaKey = key;
     await _maybeLoadLibrary();
-    await _waitForGrecaptchaReady();
+    await _waitForGrecaptchaReady(delay);
     changeVisibility(showBadge);
     debugPrint('gRecaptcha V3 ready');
-
-    if (!_completer.isCompleted) {
-      _completer.complete();
+    if (!_readyCompleter.isCompleted) {
+      _readyCompleter.complete();
     }
   }
 
-  /// TODO: Avoid using the `Future.delayed` if possible
-  static Future<void> _waitForGrecaptchaReady() async {
-    await Future.doWhile(() async {
+  static Future<void> _waitForGrecaptchaReady(Duration? delay) async {
+    if (delay != null) await Future.delayed(delay);
+    while (true) {
       final completer = Completer<bool>();
-      void complete(bool isBreak) {
+      grecaptcha.ready(() {
         if (!completer.isCompleted) {
-          completer.complete(isBreak);
+          completer.complete(true);
         }
+      }.toJS);
+      if (delay != null) {
+        final isDone = await completer.future.timeout(
+          delay,
+          onTimeout: () => false,
+        );
+        if (isDone) return;
+      } else {
+        return;
       }
-
-      // TODO: Figure out why the `grecaptcha.ready` is error even when the `grecaptcha` was loaded
-      // by checking the `globalContext.hasProperty('grecaptcha'.toJS)` variable.
-      //
-      // Caused this issue:
-      // Error
-      // dart-sdk/lib/_internal/js_dev_runtime/private/ddc_runtime/errors.dart 296:3       throw_
-      // dart-sdk/lib/_internal/js_dev_runtime/private/ddc_runtime/operations.dart 893:3   defaultNoSuchMethod
-      // dart-sdk/lib/_internal/js_dev_runtime/patch/core_patch.dart 63:17                 noSuchMethod
-      // dart-sdk/lib/_internal/js_dev_runtime/private/ddc_runtime/operations.dart 868:31  noSuchMethod
-      // dart-sdk/lib/_internal/js_dev_runtime/private/ddc_runtime/operations.dart 318:12  callNSM
-      // dart-sdk/lib/_internal/js_dev_runtime/private/ddc_runtime/operations.dart 428:10  _checkAndCall
-      // dart-sdk/lib/_internal/js_dev_runtime/private/ddc_runtime/operations.dart 431:39  dcall
-      try {
-        await grecaptcha
-            .ready(() {
-              complete(false);
-            }.toJS)
-            .toDart;
-      } catch (e) {
-        await Future.delayed(const Duration(milliseconds: 10));
-        complete(true);
-      }
-
-      return completer.future;
-    });
+    }
   }
 
   /// use `Recaptcha` not ~RecaptchaPlatformInterace~
   static Future<String?> execute(String action) async {
-    await _completer.future;
     if (!kIsWeb) return null;
-    if (_recaptchaKey == null) {
-      throw Exception('gRecaptcha V3 key not set : Try calling ready() first.');
-    }
+    await _readyCompleter.future;
     try {
       final result =
           grecaptcha.execute(_recaptchaKey!.toJS, Options(action: action.toJS));
@@ -126,40 +103,32 @@ class RecaptchaImpl {
   /// Does nothing if the library is already loaded.
   static Future<void> _maybeLoadLibrary() async {
     final completer = Completer();
-
     const scriptId = 'g_recapcha_script';
     final scriptUrl =
         'https://www.google.com/recaptcha/api.js?render=$_recaptchaKey';
-
     // Script already exists.
     if (document.querySelector('script#$scriptId') != null) {
       return;
     }
-
     final HTMLScriptElement script = HTMLScriptElement()
+      ..async = true
       ..id = scriptId
       ..src = scriptUrl;
-
     script.onerror = (JSAny _) {
       if (!completer.isCompleted) {
         completer.completeError(Exception('The G reCaptcha cannot be loaded'));
       }
     }.toJS;
-
-    document.head!.appendChild(script);
-
-    /// TODO: Avoid using the `Future.delayed` if possible
-    Future.doWhile(() async {
-      if (globalContext.hasProperty('grecaptcha'.toJS).toDart) {
-        if (!completer.isCompleted) {
-          completer.complete();
-        }
-        return false;
+    script.onload = (JSAny _) {
+      if (!completer.isCompleted) {
+        completer.complete();
       }
-      await Future.delayed(const Duration(milliseconds: 10));
-      return true;
-    });
-
+    }.toJS;
+    if (document.head != null) {
+      document.head!.appendChild(script);
+    } else {
+      document.appendChild(script);
+    }
     return completer.future;
   }
 }
